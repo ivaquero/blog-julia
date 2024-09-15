6238b3dfbdd6c3a0c52ca84fb29263bd643b286d
ivaquero <msaintms@outlook.com>
2024-09-15 23:51:25 +0800

    build: ⬆️ upgrade manifest

--- src/analysis/plots_01_corr.jl
+++ src/analysis/plots_01_corr.jl
@@ -23,13 +23,13 @@ begin
 using TidierFiles, TidierData, TidierPlots
 
 # ╔═╡ 50dcffe7-f013-4540-804f-a3badc413f62
-using WGLMakie
+using CairoMakie
 
 # ╔═╡ 07f923d3-3995-4915-a7f3-c236e6a6d0ea
 md"# Plot Correlation"
 
 # ╔═╡ 6856872e-c148-4e7e-8835-92adf9a1186b
-WGLMakie.activate!()
+CairoMakie.Makie.inline!()
 
 # ╔═╡ 8eecaeac-b0d9-4587-9a9f-61db52c31ff8
 md"## Regression Plot"
@@ -52,11 +52,20 @@ p3 = ggplot(ads, @aes(x = newspaper, y = sales)) +
      geom_point(color=:red) +
      geom_smooth(method=:lm, linewidth=3, alpha=0.5)
 
-# ╔═╡ abe2d87a-22ca-4039-b259-644ce79c98db
-p1 + p2 + p3 + plot_layout(ncol=3, nrow=1, widths=[1, 1, 1], heights=[1])
+# ╔═╡ dc0e2788-5a4a-4290-8ea3-a0556ac1ec3a
+p13 = (p1 + p2 + p3 + plot_layout(ncol=3, nrow=1, widths=[2, 2, 2], heights=[1]));
 
-# ╔═╡ 3c68ed61-afa5-4d0f-ad13-977944b1b706
-Pkg.status()
+# ╔═╡ 1f0a569a-b99f-4626-9be7-63fbfbd7f333
+p13.plots
+
+# ╔═╡ ccbb8c49-ba45-4e41-894d-cc93a6814a66
+p13.grid
+
+# ╔═╡ 2902c294-aeff-4351-a2f3-337320f9d781
+p13.grid
+
+# ╔═╡ d393be84-e5b3-4c33-a66d-f4ef3ff82a8d
+
 
 # ╔═╡ Cell order:
 # ╟─07f923d3-3995-4915-a7f3-c236e6a6d0ea
@@ -69,5 +78,8 @@ Pkg.status()
 # ╠═52d98253-dd93-4af8-b6d3-d9d2d5b9b1e2
 # ╠═efb16650-04b3-47ea-8f1f-3a2436751adc
 # ╠═3b604dd8-d398-4bab-91d1-72bd70a965dc
-# ╠═abe2d87a-22ca-4039-b259-644ce79c98db
-# ╠═3c68ed61-afa5-4d0f-ad13-977944b1b706
+# ╠═dc0e2788-5a4a-4290-8ea3-a0556ac1ec3a
+# ╠═1f0a569a-b99f-4626-9be7-63fbfbd7f333
+# ╠═ccbb8c49-ba45-4e41-894d-cc93a6814a66
+# ╠═2902c294-aeff-4351-a2f3-337320f9d781
+# ╠═d393be84-e5b3-4c33-a66d-f4ef3ff82a8d
