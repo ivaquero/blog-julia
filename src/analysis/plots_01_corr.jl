86ebc1a3f6ef68b27fd57dfb5f6f1004f6bd3a90
ivaquero <msaintms@outlook.com>
2025-01-08 13:43:50 +0800

    chore: ⬆️ update Manifest.toml

--- src/analysis/plots_01_corr.jl
+++ src/analysis/plots_01_corr.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.47
+# v0.20.4
 
 #> [frontmatter]
 #> chapter = 1
@@ -22,23 +22,8 @@ begin
 # ╔═╡ a6999fdb-d20a-4a13-b7e9-eee118eb98fa
 using TidierFiles, TidierData, TidierPlots
 
-# ╔═╡ 50dcffe7-f013-4540-804f-a3badc413f62
-using WGLMakie
-
 # ╔═╡ 07f923d3-3995-4915-a7f3-c236e6a6d0ea
 md"# Plot Correlation"
-
-# ╔═╡ 6856872e-c148-4e7e-8835-92adf9a1186b
-WGLMakie.activate!()
-
-# ╔═╡ d393be84-e5b3-4c33-a66d-f4ef3ff82a8d
-function Base.show(io::IO, ::MIME"text/html", x::TidierPlots.GGPlotGrid)
-	show(io, MIME"text/html"(),
-        with_theme(x.plots[1].theme) do
-            draw_ggplot(x)
-        end
-    )
-end
 
 # ╔═╡ 8eecaeac-b0d9-4587-9a9f-61db52c31ff8
 md"## Regression Plot"
@@ -62,22 +47,15 @@ p3 = ggplot(ads, @aes(x = newspaper, y = sales)) +
      geom_smooth(method=:lm, linewidth=3, alpha=0.5)
 
 # ╔═╡ 636af518-6bdb-4c6c-ac03-d0971dfaa479
-p13 = p1 + p2 + p3;
-
-# ╔═╡ 661926a4-4810-4be8-af66-d12416e828f3
-p13
+p13 = (p1 + p2 + p3 + plot_layout(ncol=3, nrow=1, widths=[5, 5, 5], heights=[1]))
 
 # ╔═╡ Cell order:
 # ╟─07f923d3-3995-4915-a7f3-c236e6a6d0ea
 # ╠═bbb110f4-eb0c-45e8-8456-85a951ecbfee
 # ╠═a6999fdb-d20a-4a13-b7e9-eee118eb98fa
-# ╠═50dcffe7-f013-4540-804f-a3badc413f62
-# ╠═6856872e-c148-4e7e-8835-92adf9a1186b
-# ╠═d393be84-e5b3-4c33-a66d-f4ef3ff82a8d
 # ╟─8eecaeac-b0d9-4587-9a9f-61db52c31ff8
 # ╠═902a1f74-8d10-43c7-a1eb-d090438f0a27
 # ╠═52d98253-dd93-4af8-b6d3-d9d2d5b9b1e2
 # ╠═efb16650-04b3-47ea-8f1f-3a2436751adc
 # ╠═3b604dd8-d398-4bab-91d1-72bd70a965dc
 # ╠═636af518-6bdb-4c6c-ac03-d0971dfaa479
-# ╠═661926a4-4810-4be8-af66-d12416e828f3
--- src/prob/prob_1_distributions.jl
+++ src/prob/prob_1_distributions.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.47
+# v0.20.4
 
 #> [frontmatter]
 #> chapter = 3
@@ -15,7 +15,11 @@ using InteractiveUtils
 # This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
 macro bind(def, element)
     quote
-        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local iv = try
+            Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value
+        catch
+            b -> missing
+        end
         local el = $(esc(element))
         global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
         el
