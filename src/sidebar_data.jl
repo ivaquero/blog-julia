ca69ebb5c6262e328faa3a0a36f104e82d3f7487
ivaquero <msaintms@outlook.com>
2025-07-08 15:36:39 +0800

    ci: 👷 update ci

---
+++ src/sidebar_data.jl
@@ -0,0 +1,7 @@
+Dict(
+    "main" => [
+        "Data Analysis" => collections["track_data"].pages,
+        "Scientific Computing" => collections["track_comp"].pages,
+        "Probabilistic Programming" => collections["track_prob"].pages,
+    ],
+)
